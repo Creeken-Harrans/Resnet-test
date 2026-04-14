2ee2313e8616d8de2aa023053231362e7bcbf39b
Creeken <creeken0712@gmail.com>
2026-04-14 11:00:18 +0800

    1

--- pytorch-cifar/main.py
+++ pytorch-cifar/main.py
@@ -10,6 +10,7 @@ import torchvision.transforms as transforms
 
 import os
 import argparse
+from pathlib import Path
 
 from models import SimpleDLA
 from utils import progress_bar
@@ -39,13 +40,21 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
+data_root = Path('./data')
+archive_path = data_root / 'cifar-10-python.tar.gz'
+if archive_path.exists():
+    expected_size = 170498071
+    if archive_path.stat().st_size != expected_size:
+        print(f'==> Removing corrupted archive: {archive_path}')
+        archive_path.unlink()
+
 trainset = torchvision.datasets.CIFAR10(
-    root='./data', train=True, download=True, transform=transform_train)
+    root=str(data_root), train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
     trainset, batch_size=128, shuffle=True, num_workers=2)
 
 testset = torchvision.datasets.CIFAR10(
-    root='./data', train=False, download=True, transform=transform_test)
+    root=str(data_root), train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(
     testset, batch_size=100, shuffle=False, num_workers=2)
 
